81f0cdc9acc235a424044c3cbb3dbe9cbbcb5023
Wilfred8888 <brandon.butler2@syneoshealth.com>
2019-04-05 10:59:41 -0400

    beginning app overlay mastead

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -3,7 +3,68 @@ import React from "react";
 const Overlay = props => {
   return (
     <div className={props.overlayClasses}>
-      <div className="container" style={{ height: 100 + "%" }}>
+      <div
+        className="container"
+        style={{ height: 100 + "%", position: "relative" }}
+      >
+        <div className="row masthead-row">
+          <div className="col text-center">
+            <div className="masthead-matrix">
+              <div className="row">
+                <div className="col">
+                  <div>
+                    <strong>6</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>-2</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>5</strong>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <div>
+                    <strong>1</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>0</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>1</strong>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <div>
+                    <strong>-4</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>12</strong>
+                  </div>
+                </div>
+                <div className="col">
+                  <div>
+                    <strong>0</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="row" style={{ height: 100 + "%" }}>
           <div className="col text-center align-self-center">
             <p>
